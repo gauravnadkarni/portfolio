2e98af870c6ab5a713ab466375450c0accb2a72b
Gaurav <nadkarnigaurav@gmail.com>
2024-09-26 15:41:48 +0530

    formatted the code

--- src/components/work-history/WorkHistoryBoxes.tsx
+++ src/components/work-history/WorkHistoryBoxes.tsx
@@ -1,51 +1,64 @@
+import * as React from "react";
 import MenuBookIcon from "@mui/icons-material/MenuBook";
-import MilitaryTechIcon from '@mui/icons-material/MilitaryTech';
+import MilitaryTechIcon from "@mui/icons-material/MilitaryTech";
 import SchoolIcon from "@mui/icons-material/School";
 import WorkIcon from "@mui/icons-material/Work";
 import Timeline from "@mui/lab/Timeline";
-import * as React from "react";
-import WorkHistoryBox, { WorkHistoryBoxProps, WorkHistoryBoxPropsWithIcons } from "./WorkHistoryBox";
+import WorkHistoryBox, {
+  WorkHistoryBoxProps,
+  WorkHistoryBoxPropsWithIcons,
+} from "./WorkHistoryBox";
 import { Paper } from "@mui/material";
 
 export interface WorkHistoryBoxesProps {
-  workItems: Array<WorkHistoryBoxProps>
+  workItems: Array<WorkHistoryBoxProps>;
 }
 
 const WorkHistoryBoxes: React.FC<WorkHistoryBoxesProps> = (props) => {
-  const processedData = props.workItems.map<WorkHistoryBoxPropsWithIcons>((item)=>{
-    const {dotTooltipText, tooltipPlacement, ...rest} = item;
-    let dotIcon = <WorkIcon />;
-    switch(rest.type) {
-      case "Certification": 
-        dotIcon = <MilitaryTechIcon />
-        break;
-      case "Course": 
-        dotIcon = <MenuBookIcon />
-        break;
-      case "Study": 
-        dotIcon = <SchoolIcon />
-        break;
-      default:
-        dotIcon = <WorkIcon />;
-        break;
+  const processedData = props.workItems.map<WorkHistoryBoxPropsWithIcons>(
+    (item) => {
+      const { dotTooltipText, tooltipPlacement, ...rest } = item;
+      let dotIcon = <WorkIcon />;
+      switch (rest.type) {
+        case "Certification":
+          dotIcon = <MilitaryTechIcon />;
+          break;
+        case "Course":
+          dotIcon = <MenuBookIcon />;
+          break;
+        case "Study":
+          dotIcon = <SchoolIcon />;
+          break;
+        default:
+          dotIcon = <WorkIcon />;
+          break;
+      }
+
+      return {
+        ...rest,
+        dotIcon,
+        dotTooltip: <div>{dotTooltipText}</div>,
+        tooltipPlacement:
+          tooltipPlacement === "left" ? "bottom-end" : "bottom-start",
+      };
     }
+  );
 
-    return {
-      ...rest,
-      dotIcon,
-      dotTooltip: <div>{dotTooltipText}</div>,
-      tooltipPlacement: tooltipPlacement === "left" ? "bottom-end" : "bottom-start",
-    }
-  });
-
-    return (
-      <Paper elevation={3} sx={{py:'30px', borderRadius:"10px"}}>
-        <Timeline position="alternate">
-          {processedData.map((item,idx) => (<WorkHistoryBox  {...item}/>))}
-        </Timeline>
-      </Paper>
-    );
-  };
-
+  return (
+    <Paper
+      elevation={3}
+      sx={{
+        py: "30px",
+        borderRadius: "10px",
+      }}
+    >
+      <Timeline position="alternate">
+        {processedData.map((item, idx) => (
+          <WorkHistoryBox {...item} />
+        ))}
+      </Timeline>
+    </Paper>
+  );
+};
 
 export default WorkHistoryBoxes;
